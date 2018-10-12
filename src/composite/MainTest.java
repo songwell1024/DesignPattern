9d9d2b5476ec0f6b30a26ea18f76b594bbfc7598
songwell1024 <zysong0709@foxmail.com>
2018-10-12 21:28:38 +0800

    add composite mode

---
+++ src/composite/MainTest.java
@@ -0,0 +1,12 @@
+package composite;
+
+public class MainTest {
+	public static void main(String[] args) {
+		Waitress mWaitress = new Waitress();
+		CakeHouseMenu mCakeHouseMenu = new CakeHouseMenu();
+		DinerMenu mDinerMenu = new DinerMenu();
+		mWaitress.addComponent(mCakeHouseMenu);
+		mWaitress.addComponent(mDinerMenu);
+		mWaitress.printVegetableMenu();;
+	}
+}
